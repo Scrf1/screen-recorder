06e4633cb1a27c0a878ad2223f57f4787408735e
Scrf1 <srgseraphin14@gmail.com>
2022-10-09 23:35:14 +0100

    Adding main classes

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,9 @@
+import gui.MainGUI;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        MainGUI mainGUI = new MainGUI();
+    }
+
+}
---
+++ src/main/java/gui/MainGUI.java
@@ -0,0 +1,13 @@
+package gui;
+
+import java.awt.*;
+
+public class MainGUI extends Frame {
+
+    public MainGUI() {
+        super("JRecorder");
+        setSize(450, 300);
+        setLayout(new GridLayout());
+        setVisible(true);
+    }
+}
