9bebbbe2f358f8c2c1bd87c3ca59282993b3aa30
SCrf1 <srgseraphin14@gmail.com>
2022-10-19 23:46:13 +0200

    Creating button apps

--- src/main/java/gui/containers/VideoRecorderPanel.java
+++ src/main/java/gui/containers/VideoRecorderPanel.java
@@ -1,10 +1,28 @@
 package gui.containers;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VideoRecorderPanel extends JPanel {
 
+    private JButton play;
+    private JButton pause;
+    private JButton stop;
+    private JLabel elapsedTime;
+
     public VideoRecorderPanel() {
+        play = new JButton("Play");
+        pause = new JButton("Pause");
+        stop = new JButton("Stop");
+
+        elapsedTime = new JLabel("00:00:00");
+
+        //this.setLayout(new BorderLayout());
+        this.add(play);
+        this.add(pause);
+        this.add(stop);
+
+        this.add(elapsedTime);
     }
 
 }
